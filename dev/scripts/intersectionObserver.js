9d935d4842566a69e416f8d4e10dcdee930ccc7c
Emmanuel Albania <ejalbania@gmail.com>
2025-02-13 10:33:08 +0800

    Updated observer

--- dev/scripts/intersectionObserver.js
+++ dev/scripts/intersectionObserver.js
@@ -30,3 +30,62 @@ const observer = new IntersectionObserver(
   },
   { rootMargin: "80px" }
 );
+
+const leftTransition = document.querySelectorAll(".left-transition");
+const rightTransition = document.querySelectorAll(".right-transition");
+const fadeInTransition = document.querySelectorAll(".fade-in-transition");
+const steppingNumber = document.querySelectorAll(".stepping-number");
+
+leftTransition.forEach((element) => {
+  element.classList.add("slide-in-from-left-animated");
+  observer.observe(element);
+
+  element.classList.remove("left-transition");
+});
+
+rightTransition.forEach((element) => {
+  element.classList.add("slide-in-from-right-animated");
+  observer.observe(element);
+
+  element.classList.remove("right-transition");
+});
+
+fadeInTransition.forEach((element) => {
+  element.classList.add("fade-in-animated");
+  observer.observe(element);
+
+  element.classList.remove("fade-in-transition");
+});
+
+steppingNumber.forEach((element) => {
+  observer.observe(element);
+});
+
+function stepper(
+  min,
+  max,
+  duration,
+  increment = 1,
+  callback,
+  completion = () => {}
+) {
+  var current = min;
+
+  var timer = setInterval(() => {
+    current += increment;
+
+    if (current >= max) {
+      callback(max);
+      clearInterval(timer);
+      completion();
+    } else {
+      callback(current);
+    }
+  }, duration);
+}
+
+function numberStepper(element, value, increment) {
+  stepper(0, value, 32, increment, (count) => {
+    element.textContent = count;
+  });
+}
